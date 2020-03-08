cd41d42c224ea6b23e3a0b6c0bb0b25db586550e
ops_syy <577097213@qq.com>
2020-03-08 21:37:00 +0800

    monitoring.py--commit--20200308

---
+++ python/monitoring.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import paramiko
+import time
+
+lists=[["****", "*", "***", "****"]]
+
+"""
+连接服务器方法
+"""
+
+
+def ssh_client(host,port,username,passwd):
+
+    ssh=paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, username, passwd)
+    string = "ps -aux|grep java|grep -v grep|awk '{print \"pid:\", $2, \"cpu:\", $3, \"mem:\", $4}'"
+    stdin, stdout, stderr=ssh.exec_command(string)
+
+    i = 0
+    counts = stdout.readlines()
+    for count in counts:
+        print(count)
+        i = i+1
+    print("java进程数量：" + str(i))
+    ssh.close()
+
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(5)
+        for information in lists:
+            try:
+                print("//////////////////////////////////")
+                print("连接服务器："+information[0])
+                ssh_client(information[0],information[1],information[2],information[3])
+            except RuntimeError:
+                print("运行错误")
--- python/test2.py
+++ python/test2.py
@@ -1,15 +1,7 @@
-ssh.connect("124.160.108.60",60000,"root", "dahuafire")
-if __name__ == '__main__':
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
-    for host in host_lists:
-        try:
-            info = getServerInfo(host[0],host[1],host[2],host[3])
-            a = host[0]
-            b = host[1]
-            c = host[2]
-            d = host[4]
-            print(''.join(a),''.join(b))
+"""
+ceshi
 
-        except:
-            pass
-
+"""
